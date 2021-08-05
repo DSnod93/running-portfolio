6dbfc39ed72de2027a608b62f2586e2d8dfee623
DSnoddy <douglassnoddyjr@gmail.com>
2021-08-04 16:15:32 -0800

    update with folders

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const projects = [
+    {
+      title: "Cuisine-Time",
+      subtitle: "HTML, CSS, JavaScript",
+      description:
+        "A web application that allows any user to search for restaurants in his /her location based on user's cuisine of choice sorted by highest rating. ",
+      image: "./project-1.jpg",
+      link: "https://dsnod93.github.io/Cuisine-Time/",
+    },
+    {
+      title: "DishADay",
+      subtitle: "Javascript, CSS, Mysql2, Sequelize, bcrypt, Materialize, Express, Node",
+      description:
+        "A user-friendly site where user can write different food topics and create amazing contents by publishing and adding comments to other posts as well.",
+      image: "./project-2.jpg",
+      link: "https://dishaday.herokuapp.com/",
+    },
+    {
+      title: "The Food Chain",
+      subtitle: "MERN Stack, Materialize, GraphQL",
+      description:
+        "The goal of this application is to create a mobile-friendly e-commerce/grocery app using MERN and GraphQL.",
+      image: "./project-3.jpg",
+      link: "https://thefoodchain.herokuapp.com/",
+    },
+    {
+      title: "The Note Taker",
+      subtitle: "HTML, CSS, JavaScript, Node",
+      description:
+        "The goal for this project is to create an application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.",
+      image: "./mini-project.jpg",
+      link: "https://pure-ocean-79554.herokuapp.com/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://randomuser.me/api/portraits/men/1.jpg",
+      name: "Jesse Hicks",
+      company: "Zoozle",
+    },
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://randomuser.me/api/portraits/men/94.jpg",
+      name: "Ruben Alvarez",
+      company: "Dooble.io",
+    },
+  ];
+  
+  export const skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Boostrap",
+    "Materialize",
+    "Responsive Design",
+    "React",
+    "API",
+    "Node",
+    "MongoDB",
+    "Mongoose",
+    "Express",
+    "MySQL",
+    "Sequelize",
+    "Rest"
+  ];
+  
